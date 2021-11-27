5d046a67590f6d00b74456889184cc63374bb524
Rohan Jain <crodjer@pm.me>
2021-11-27 18:01:32 +0530

    Update package dependencies

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use clap::Clap;
+use clap::Parser;
 use std::io::stdout;
 use std::io::Result;
 use std::io::Write;
